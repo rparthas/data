ac552ad52578921cf09bcfe6526874fa87e445a7
Rajagopal Parthasarathi <psraja@thoughtworks.com>
2020-03-01 18:31:03 +0530

    change folder name

--- PySpark/test/edu/test_spark.py
+++ PySpark/test/edu/test_spark.py
@@ -8,21 +8,21 @@ class TestSpark(unittest.TestCase):
         self.demo = spark.Demo()
 
     def test_load_text_file(self):
-        count = self.demo.load_text_file("data/sampletext.txt")
+        count = self.demo.load_text_file("input/sampletext.txt")
         self.assertEqual(128457, count)
 
     def test_load_csv_file(self):
-        count = self.demo.load_csv_file("data/samplecsv.csv")
+        count = self.demo.load_csv_file("input/samplecsv.csv")
         self.assertEqual(93, count)
 
     def test_write_csv_file(self):
-        count = self.demo.write_csv_file("data/samplecsv.csv", "output/sample.csv")
+        count = self.demo.write_csv_file("input/samplecsv.csv", "output/sample.csv")
         self.assertEqual(93, count)
 
     def test_write_parquet_file(self):
-        count = self.demo.write_parquet_file("data/samplecsv.csv", "output/sample.parquet")
+        count = self.demo.write_parquet_file("input/samplecsv.csv", "output/sample.parquet")
         self.assertEqual(93, count)
 
     def test_using_sql(self):
-        count = self.demo.test_sql("data/samplecsv.csv")
+        count = self.demo.test_sql("input/samplecsv.csv")
         self.assertEqual(93, count)
