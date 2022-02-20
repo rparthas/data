e1d9109dc11fa3e503c1cad8f784c6ccc0de2948
rajagopalps <rajagopal.sarathi@gmail.com>
2022-02-20 12:55:19 +0530

    Week 2 with Homework

--- de-zoomcamp/week2/airflow/dags/yellow_taxi_gcs_dag.py
+++ de-zoomcamp/week2/airflow/dags/yellow_taxi_gcs_dag.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from datetime import datetime
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -7,18 +8,18 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from google.cloud import storage
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
-path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace('.csv', '.parquet')
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+URL_PREFIX = 'https://s3.amazonaws.com/nyc-tlc/trip+data'
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+YELLOW_TAXI_URL_TEMPLATE = URL_PREFIX + "/yellow_tripdata_{{execution_date.strftime('%Y-%m')}}.csv"
+YELLOW_TAXI_CSV_FILE_TEMPLATE = AIRFLOW_HOME + "/yellow_tripdata_{{execution_date.strftime('%Y-%m')}}.csv"
+YELLOW_TAXI_PARQUET_FILE = "yellow_tripdata_{{execution_date.strftime('%Y-%m')}}.parquet"
+YELLOW_TAXI_PARQUET_GCP_FILE = "raw/yellow_trip_data/" + YELLOW_TAXI_PARQUET_FILE
 
 
 def format_to_parquet(src_file):
@@ -60,23 +61,24 @@ default_args = {
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
-        dag_id="data_ingestion_gcs_dag",
-        schedule_interval="@daily",
+        dag_id="yellow_taxi_gcs_dag",
+        schedule_interval="0 6 2 * *",
         default_args=default_args,
+        start_date=datetime(2019, 1, 1),
         catchup=False,
-        max_active_runs=1,
+        max_active_runs=3,
         tags=['dtc-de'],
 ) as dag:
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
+        bash_command=f"curl -sSfL {YELLOW_TAXI_URL_TEMPLATE} > {YELLOW_TAXI_CSV_FILE_TEMPLATE}"
     )
 
     format_to_parquet_task = PythonOperator(
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
+            "src_file": f"{YELLOW_TAXI_CSV_FILE_TEMPLATE}",
         },
     )
 
@@ -86,24 +88,14 @@ with DAG(
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"{YELLOW_TAXI_PARQUET_GCP_FILE}",
+            "local_file": f"{YELLOW_TAXI_CSV_FILE_TEMPLATE.replace('.csv', '.parquet')}",
         },
     )
 
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-            },
-        },
+    rm_task = BashOperator(
+        task_id="rm_dataset_task",
+        bash_command=f"rm  {YELLOW_TAXI_CSV_FILE_TEMPLATE} {YELLOW_TAXI_CSV_FILE_TEMPLATE.replace('.csv', '.parquet')}"
     )
 
-    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_external_table_task
+    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> rm_task
